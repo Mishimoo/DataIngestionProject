d5585b023dbf7f7be27c6bbc75d6daf2fa77d298
Mishimoo <mishra.ryan.p@gmail.com>
2026-02-10 14:11:08 -0500

    analysis added

---
+++ main.py
@@ -0,0 +1,35 @@
+import logging
+from ingestion import *
+from pathlib import Path
+
+def main():
+    # Logger for the Ingestion 
+    Path("logs").mkdir(exist_ok=True)
+
+    logging.basicConfig(
+        filename="logs/ingestion.log",
+        filemode="a",
+        format="%(process)d - %(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+        level=logging.DEBUG
+    )
+
+    logger = logging.getLogger("main")
+    logger.info("Ingestion pipeline starting")
+
+    # our mental health db
+    PROJECT_ROOT = Path(__file__).resolve().parent
+    data_path = PROJECT_ROOT / "data" / "Mental_Health_DB.csv"
+    #turns the csv into a dataframe
+    df = read_data(data_path)
+    #takes the dataframe and turns it into a valid and a rejected dataframe
+    valid, rejected = retrieve_data(df)
+    #cleans the valid data to be stored int he database
+    valid = clean_data(valid)
+    valid.to_csv("data/valid_data.csv", index= False)
+    #loads valid into database
+    #loader method goes here
+
+
+if __name__ == "__main__":
+    main()
