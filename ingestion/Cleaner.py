2b05947b44424ac3a8d9b148ae4576da95122ae3
Mishimoo <mishra.ryan.p@gmail.com>
2026-02-10 14:13:26 -0500

    cleaner updated

--- ingestion/Cleaner.py
+++ ingestion/Cleaner.py
@@ -1,46 +1,45 @@
 import os
 import pandas as pd
+import logging
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CSV_PATH = os.path.join(BASE_DIR, "..", "data", "Raw Data", "Mental_Health_DB.csv")
-
-print("Trying path:", CSV_PATH)
-print("Exists?", os.path.exists(CSV_PATH))
-
-df = pd.read_csv(CSV_PATH)
-print(df.dtypes)
+logger = logging.getLogger(__name__)
 
 def clean_data(df):
+    logger.info("Starting data cleaning step")
+    
+    original_rows = len(df)
+
     # --- Remove duplicates ---
     df = df.drop_duplicates()
+    logger.debug(f"Removed {original_rows - len(df)} duplicate rows")
 
     # --- Trim strings ---
     df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
 
     # --- Enforce numeric types ---
-    numeric_cols = ["Phase", "Value", "LowCI", "HighCI"]#idk about the last three
+    numeric_cols = ["phase", "value", "lowci", "highci"]#idk about the last three
     for col in numeric_cols:
         df[col] = pd.to_numeric(df[col], errors="coerce")
+        logger.debug(f"Converted column '{col}' to numeric")
 
     # --- Enforce int type ---
-    df["Time Period"] = pd.to_numeric(df["Time Period"], errors="coerce").astype("Int64")
+    df["time period"] = pd.to_numeric(df["time period"], errors="coerce").astype("Int64")
+    logger.debug("Converted 'time period' to nullable Int64")
 
     # --- Enforce datetime ---
-    df["Time Period Start Date"] = pd.to_datetime(df["Time Period Start Date"], errors="coerce")
-    df["Time Period End Date"] = pd.to_datetime(df["Time Period End Date"], errors="coerce")
+    df["time period start date"] = pd.to_datetime(df["time period start date"], errors="coerce")
+    logger.debug(f"Converted 'time period start date' to datetime")
+    df["time period end date"] = pd.to_datetime(df["time period end date"], errors="coerce")
+    logger.debug(f"Converted 'time period end date' to datetime")
 
     # --- Normalize text columns ---
     text_cols = [
-        "Indicator", "Group", "State", "Subgroup",
-        "Time Period Label", "Confidence Interval", "Quartile Range"
+        "indicator", "group", "state", "subgroup",
+        "time period label", "confidence interval", "quartile range"
     ]
     for col in text_cols:
         df[col] = df[col].astype("string")
-    
-    print("\nAfter type enforcement:")
-    print(df.dtypes)
+        logger.debug(f"Converted '{col}' to string")
 
-
+    logger.info(f"Completed data cleaning.")
     return df
-df_clean = clean_data(df)
-
